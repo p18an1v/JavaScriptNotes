6b9fb74c2eecf69cb0a4ebd26c11f69278128be8
p18an1v <pranavmhargude@gmail.com>
2023-09-25 19:29:41 +0530

    Initial commit

---
+++ Classes/NewExp.js
@@ -0,0 +1,14 @@
+function fun() {
+    alert("welocme");
+}
+
+
+//Arrow function in modern jaavscript:
+
+sum = () => {
+    console.log("hello world");
+}
+
+sum1 = (p, q) => {
+    console.log(p + q);
+}
